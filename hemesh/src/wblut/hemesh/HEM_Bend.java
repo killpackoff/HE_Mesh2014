3a99b68e1c0048e7266507a7f281a6d0d9a25ad2
killpack <killpackoff@gmail.com>
2015-08-26 15:54:53 +0300

    Stabilizate

--- hemesh/src/wblut/hemesh/HEM_Bend.java
+++ hemesh/src/wblut/hemesh/HEM_Bend.java
@@ -1,6 +1,7 @@
 package wblut.hemesh;
 
 import java.util.Iterator;
+
 import wblut.geom.WB_GeometryOp;
 import wblut.geom.WB_Line;
 import wblut.geom.WB_Plane;
@@ -9,35 +10,41 @@ import wblut.geom.WB_Plane;
  * Bend a mesh. Determined by a ground plane, a bend axis and an angle factor.
  *
  * @author Frederik Vanhoutte (W:Blut)
- *
  */
 public class HEM_Bend extends HEM_Modifier {
-    /** Ground plane. */
+    /**
+     * Ground plane.
+     */
     private WB_Plane groundPlane;
-    /** Bend axis. */
+    /**
+     * Bend axis.
+     */
     private WB_Line bendAxis;
-    /** Angle factor. */
+    /**
+     * Angle factor.
+     */
     private double angleFactor;
-    /** Positive side of plane only?. */
+    /**
+     * Positive side of plane only?.
+     */
     private boolean posOnly;
 
     /**
      * Instantiates a new HEM_Bend.
      */
     public HEM_Bend() {
-	super();
+        super();
     }
 
     /**
      * Set ground plane.
      *
-     * @param P
-     *            ground plane
+     * @param P ground plane
      * @return self
      */
     public HEM_Bend setGroundPlane(final WB_Plane P) {
-	groundPlane = P;
-	return this;
+        groundPlane = P;
+        return this;
     }
 
     /**
@@ -46,58 +53,55 @@ public class HEM_Bend extends HEM_Modifier {
      * @return self
      */
     public HEM_Bend setGroundPlane(final double ox, final double oy,
-	    final double oz, final double nx, final double ny, final double nz) {
-	groundPlane = new WB_Plane(ox, oy, oz, nx, ny, nz);
-	return this;
+                                   final double oz, final double nx, final double ny, final double nz) {
+        groundPlane = new WB_Plane(ox, oy, oz, nx, ny, nz);
+        return this;
     }
 
     /**
      * Set bend axis.
      *
-     * @param a
-     *            bend axis
+     * @param a bend axis
      * @return self
      */
     public HEM_Bend setBendAxis(final WB_Line a) {
-	bendAxis = a;
-	return this;
+        bendAxis = a;
+        return this;
     }
 
     /**
      * Sets the bend axis.
      *
      * @return self
-     * */
+     */
     public HEM_Bend setBendAxis(final double p1x, final double p1y,
-	    final double p1z, final double p2x, final double p2y,
-	    final double p2z) {
-	bendAxis = new WB_Line(p1x, p1y, p1z, p2x - p1x, p2y - p1y, p2z - p2y);
-	return this;
+                                final double p1z, final double p2x, final double p2y,
+                                final double p2z) {
+        bendAxis = new WB_Line(p1x, p1y, p1z, p2x - p1x, p2y - p1y, p2z - p2y);
+        return this;
     }
 
     /**
      * Set angle factor, ratio of bend angle in degrees to distance to ground
      * plane.
      *
-     * @param f
-     *            direction
+     * @param f direction
      * @return self
      */
     public HEM_Bend setAngleFactor(final double f) {
-	angleFactor = f * (Math.PI / 180);
-	return this;
+        angleFactor = f * (Math.PI / 180);
+        return this;
     }
 
     /**
      * Positive only? Only apply modifier to positive side of ground plane.
      *
-     * @param b
-     *            true, false
+     * @param b true, false
      * @return self
      */
     public HEM_Bend setPosOnly(final boolean b) {
-	posOnly = b;
-	return this;
+        posOnly = b;
+        return this;
     }
 
     /*
@@ -107,19 +111,18 @@ public class HEM_Bend extends HEM_Modifier {
      */
     @Override
     public HE_Mesh apply(final HE_Mesh mesh) {
-	if ((groundPlane != null) && (bendAxis != null) && (angleFactor != 0)) {
-	    HE_Vertex v;
-	    final Iterator<HE_Vertex> vItr = mesh.vItr();
-	    while (vItr.hasNext()) {
-		v = vItr.next();
-		final double d = WB_GeometryOp.getDistance3D(v, groundPlane);
-		if (!posOnly || (d > 0)) {
-		    v.getPoint().rotateAboutAxis(d * angleFactor,
-			    bendAxis.getOrigin(), bendAxis.getDirection());
-		}
-	    }
-	}
-	return mesh;
+        if ((groundPlane != null) && (bendAxis != null) && (angleFactor != 0)) {
+            HE_Vertex v;
+            final Iterator<HE_Vertex> vItr = mesh.vItr();
+            while (vItr.hasNext()) {
+                v = vItr.next();
+                final double d = WB_GeometryOp.getDistance3D(v, groundPlane);
+                if (!posOnly || (d > 0)) {
+                    v.getPoint().rotateAboutAxis(d * angleFactor,bendAxis.getOrigin(), bendAxis.getDirection());
+                }
+            }
+        }
+        return mesh;
     }
 
     /*
@@ -130,18 +133,18 @@ public class HEM_Bend extends HEM_Modifier {
      */
     @Override
     public HE_Mesh apply(final HE_Selection selection) {
-	if ((groundPlane != null) && (bendAxis != null) && (angleFactor != 0)) {
-	    HE_Vertex v;
-	    final Iterator<HE_Vertex> vItr = selection.vItr();
-	    while (vItr.hasNext()) {
-		v = vItr.next();
-		final double d = WB_GeometryOp.getDistance3D(v, groundPlane);
-		if (!posOnly || (d > 0)) {
-		    v.getPoint().rotateAboutAxis(d * angleFactor,
-			    bendAxis.getOrigin(), bendAxis.getDirection());
-		}
-	    }
-	}
-	return selection.parent;
+        if ((groundPlane != null) && (bendAxis != null) && (angleFactor != 0)) {
+            HE_Vertex v;
+            final Iterator<HE_Vertex> vItr = selection.vItr();
+            while (vItr.hasNext()) {
+                v = vItr.next();
+                final double d = WB_GeometryOp.getDistance3D(v, groundPlane);
+                if (!posOnly || (d > 0)) {
+                    v.getPoint().rotateAboutAxis(d * angleFactor,
+                            bendAxis.getOrigin(), bendAxis.getDirection());
+                }
+            }
+        }
+        return selection.parent;
     }
 }
